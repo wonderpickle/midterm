f5da144acd00b114beabe36c1e844109316ccdfb
Nay Naing <42847131+wonderpickle@users.noreply.github.com>
2018-10-14 14:29:14 -0400

    Add files via upload

--- color.js
+++ color.js
@@ -5,51 +5,66 @@ var src = document.getElementById("background");
 
 
 
-var words=['purple', 'pink','yellow','red','blue','green'];
-var s;
-var answerArray = [];
 
+var words=['purple', 'pink','yellow','red','blue','green','orange','violet','turquoise','gold','lime','aqua','navy','coral','teal','brown','white','black'];	
 var word = words[Math.floor(Math.random() * words.length)];
-console.log(word);
+var singleLetter = word.split("");
+const charsList = document.querySelector("underscore");
+var x = " X"
+var answerArray = [];
+var wrongArray =[];
+console.log(singleLetter);
 
 
-
-
-	for ( var i=0;i<word.length;i++){
+// generating underscores
+for ( var i=0;i<word.length;i++){
 
 		answerArray[i]="_";
 		console.log(answerArray[i]);
 
 		
-	s = answerArray.join(" ");
-	document.getElementById('underscore').innerHTML = s;
+var	s = answerArray.join(" ");
+	document.getElementById('update').innerHTML = s;
+}	
 
-}
-	console.log(s);	
+	var lives =5;
+	var textbox= document.getElementById('letter');
+	textbox.addEventListener("keyup", function(){
 
-function Letter(){
-	var letter = document.getElementById('letter').value;
+	    var input = this.value;
 
-	if(letter.length>0){
-		for(var i=0; i<randomWord.length;i++){
-			if (words[i]===letter){
-				answerArray[i] = letter;
-			}
-			count++;
-		}
-	}
-}
+	    console.log(singleLetter.indexOf(input));
 
-	
-var remainingLetters = word.length;
+	  // if the result of indexof is less than zero == you are wrong
+	    // else 
 
-console.log(remainingLetters);
+	    for(var j =0; j<singleLetter.length;j++){
+	    	if(singleLetter[j] === input){
+	    		console.log('right')
+	    		answerArray[j]=input;
+				document.getElementById('update').innerHTML = input;
+	    	} else {
+	    		console.log('wrong')
+	    		wrongArray[j]=input;
+				document.getElementById('wrongLetters').innerHTML=  input ;
+				document.getElementById('xes').innerHTML = x ;
+				lives--;
+	    	}
+
+	    }
+	    
+			}, false);
+
+var remainingLetters = word.length;	
+
+
+
 
  // The game loop
  /*
  while (remainingLetters > 0) {
  // Show the player their progress
-/ alert(answerArray.join(" "));
+ alert(answerArray.join(" "));
  // Get a guess from the player
  var guess = prompt("Guess a letter, or click Cancel to stop playing.");
  if (guess === null) {
@@ -66,8 +81,8 @@ console.log(remainingLetters);
  }
  }
  }
-  }
+  /*/
  // Show the answer and congratulate the player
  //alert(answerArray.join(" "));
  //alert("Good job! The answer was " + word);
-*/
+
